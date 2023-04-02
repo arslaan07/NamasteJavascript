06d64655d470f6674e72c70f2510ffe5805bb5be
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-04-02 20:41:15 +0530

    Ep 3 done

---
+++ Ep. 3/index.js
@@ -0,0 +1,11 @@
+// even before code is executed, memory is allocated
+// to all variablkes and functions
+
+getName();
+console.log(getName);
+console.log(x);
+var x = 7;
+var getName = () => {
+  console.log("Namaste Javascript");
+}
+
