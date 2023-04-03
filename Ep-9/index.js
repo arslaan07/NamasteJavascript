dbee9d6c59a6561f342a9839becd10ed2ffd9a7e
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-04-03 16:49:38 +0530

    day9 done

---
+++ Ep-9/index.js
@@ -0,0 +1,43 @@
+
+        // Block Scope
+// {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+//  console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ----------------------------------------------------------------------
+
+//     var a = 100;
+// {
+//     var a = 10;  //this a shadows the global a outside block
+//     let b = 20;
+//     const c = 30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+//  console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ----------------------------------------------------------------------------
+
+let b = 100; //this b shadows the inner a in block
+{
+    var a = 10;  
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
